1ea517ff5c78047e113df0a7570a49006e25640f
Ostyslav <140643005+ostyslava@users.noreply.github.com>
2026-03-06 15:03:14 +0100

    Extend BigBundleCancelTextTooltipMixin to Block class

--- src/main/java/net/bigbundlemixin/mixin/BigBundleCancelTextTooltipMixin.java
+++ src/main/java/net/bigbundlemixin/mixin/BigBundleCancelTextTooltipMixin.java
@@ -2,7 +2,8 @@ package net.bigbundlemixin.mixin;
 
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.component.ItemContainerContents;
@@ -13,7 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.List;
 
 @Mixin(targets = "net.bigbundlemod.block.BigBundleBlock", remap = false)
-public class BigBundleCancelTextTooltipMixin {
+public abstract class BigBundleCancelTextTooltipMixin extends Block {
+
+    protected BigBundleCancelTextTooltipMixin(BlockBehaviour.Properties properties) {
+        super(properties);
+    }
+
     @Inject(method = "appendTooltip", at = @At("HEAD"), cancellable = true)
     private void replaceTooltip(ItemStack stack, Object context,
                                  List<Component> tooltip, Object flag,
